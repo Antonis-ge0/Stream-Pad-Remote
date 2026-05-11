94d1bd1dfef9f2d71ae3aa0fcffa24b12bff4d8a
Antonis Geo <antgeoath@csd.auth.gr>
2026-05-11 04:33:54 +0300

    Update release URLs for Stream Pad Remote repo

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,10 +1,10 @@
 export const APP_CONFIG = {
   name: "Stream Pad Remote",
-  version: "v0.1.6",
+  version: "v0.1.7",
   feedbackFormUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSeNMxf6YHH9jKiOnXKVtkDlyra7X9zx19LP-UiTwLGW3kAkkA/viewform",
   latestReleaseApiUrl:
-    "https://api.github.com/repos/Antonis-ge0/Stream-Deck-Remote/releases/latest",
+    "https://api.github.com/repos/Antonis-ge0/Stream-Pad-Remote/releases/latest",
   latestReleaseUrl:
-    "https://github.com/Antonis-ge0/Stream-Deck-Remote/releases/latest",
+    "https://github.com/Antonis-ge0/Stream-Pad-Remote/releases/latest",
 } as const;
